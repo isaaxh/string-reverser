cf37fcbe392e1d707a595a803c5f81608ea39874
Isaac Hussain <isaaclearns58@gmail.com>
2023-05-31 20:43:50 +0300

    add break the string into an array

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [output, setOutput] = useState();
+  const [inputString, setInputString] = useState<string>();
+  const [output, setOutput] = useState<string>();
+  const [stringArray, setStringArray] = useState<string[] | undefined>([]);
+
+  const reverseString = (strArr: string[]) => {
+    console.log(strArr);
+  };
+
+  const handleBtnClick = (inputText: string | undefined) => {
+    const newArray = inputText?.split("");
+    setStringArray(newArray);
+  };
+
+  useEffect(() => {
+    console.log(stringArray);
+  }, [stringArray]);
+
   return (
     <div className='container'>
       <div className='question-container'>
         <h1 className='title'>Reverse the String</h1>
         <div className='input-container'>
-          <input className='input-text' type='text' />
+          <input
+            className='input-text'
+            type='text'
+            onChange={(e) => setInputString(e.target.value)}
+            placeholder='Enter a text'
+          />
         </div>
       </div>
       <div className='output-container'>
         <h1 className='title'>Output</h1>
-        <div className='output'>hello</div>
+        <div className='output'>{output}</div>
       </div>
       <div className='btn-container'>
-        <button className='btn-reverse' type='button' onClick={() => {}}>
+        <button
+          className='btn-reverse'
+          type='button'
+          onClick={() => handleBtnClick(inputString)}
+        >
           Reverse String
         </button>
       </div>
